50e66351e3c96c089bc2223759e92737c68321e4
AHMAD NADEEM <113020323+ahmadnadeem-12@users.noreply.github.com>
2025-09-06 20:09:30 +0500

    Update script.js

--- script.js
+++ script.js
@@ -4,16 +4,34 @@ const btnInvalid=document.querySelector("#invalid")
 
 const toastBox=document.querySelector(".toast-box")
 
-let SuccessMsg=` <i id="success-icon" class="fa-solid fa-circle-check"></i>`;
+let successMsg=`<i class="fa-solid fa-circle-check"></i> Successfully Submitted. `
+let errorMsg=`<i class="fa-solid fa-circle-xmark"></i> Please Fix the error!`
+let invalidMsg=`<i class="fa-solid fa-circle-exclamation"></i> Invalid input, check again. `
 
-let btn=()=>{
+function Showing(msg){
     let toast=document.createElement("div")
-    let icon = document.createElement(SuccessMsg);
-    let desc=document.createElement(span)
-    toast.appendChild(icon,desc)
+    toast.classList.add('toast')
+    toast.innerHTML=msg;
+    toastBox.appendChild(toast);
+
+    if(msg.includes("error")){
+        toast.classList.add("error")
+    }
+    if(msg.includes("Invalid")){
+        toast.classList.add("invalid")
+    }
     
+    setTimeout(() => {
+        toast.remove()
+    }, 5000);
 }
 
 btnSuccess.addEventListener("click",()=>{
-
+    Showing(successMsg)
+})
+btnError.addEventListener("click",()=>{
+    Showing(errorMsg)
+})
+btnInvalid.addEventListener("click",()=>{
+    Showing(invalidMsg)
 })
